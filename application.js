5c57922773a11824a934553297625617980c580f
Jeremy Kross <jeremy.kross@containership.io>
2016-11-09 16:25:52 +0000

    More updates for PR.

--- application.js
+++ application.js
@@ -11,45 +11,45 @@ module.exports = new ContainershipPlugin({
         core.logger.register(applicationName);
 
         const addApplication = () => {
-            request({
-                baseUrl: `http://localhost:${core.options['api-port']}`,
-                url: `/v1/applications/${applicationName}`,
-                method: 'POST',
-                json: {
-                    id: applicationName,
-                    image: "containership/ntp:latest",
-                    cpus: 0.1,
-                    memory: 16,
-                    privileged: true,
-                    tags: {
-                        constraints: {
-                            per_host: 1
+            const key = [core.constants.myriad.APPLICATION_PREFIX, applicationName].join(core.constants.myriad.DELIMITER);
+
+            core.cluster.myriad.persistence.get(key, (err) => {
+                if(err) {
+                    core.applications.add({
+                        id: applicationName,
+                        image: 'jeremykross/ntp:latest',
+                        cpus: 0.1,
+                        memory: 16,
+                        privileged: true,
+                        tags: {
+                            constraints: {
+                                per_host: 1
+                            },
+                            metadata: {
+                                plugin: applicationName,
+                                ancestry: 'containership.plugin'
+                            }
                         },
-                        metadata: {
-                            plugin: applicationName,
-                            ancestry: 'containership.plugin'
+                    }, (err) => {
+                        if(!err) {
+                            core.loggers[applicationName].log('verbose', `Created ${applicationName}!`);
+                        } else {
+                            core.loggers[applicationName].log('verbose', `Couldn't create ${applicationName}: ${JSON.stringify(err)}`);
                         }
-                    },
-                }
-            }, (err, res) => {
-                if(!err) {
-                    core.loggers[applicationName].log("verbose", `Created ${applicationName}: ${JSON.stringify(res)}`);
+                    });
                 } else {
-                    core.loggers[applicationName].log("verbose", `Couldn't create ${applicationName}: ${JSON.stringify(err)}`);
-
+                    core.loggers[applicationName].log('verbose', `${applicationName} already exists, skipping create!`);
                 }
             });
+        };
+
+        if(core.cluster.praetor.is_controlling_leader()) {
+            addApplication();
         }
 
-        if(core.api) {
-            if(core.cluster.praetor.is_controlling_leader()) {
-                addApplication();
-            }
-
-            core.cluster.legiond.on("promoted", () => {
-                addApplication();
-            });
-        }
+        core.cluster.legiond.on('myriad.bootstrapped', () => {
+            addApplication();
+        });
     },
 
     reload: function() {}
